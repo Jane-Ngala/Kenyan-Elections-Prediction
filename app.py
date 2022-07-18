666d8d9a018da40207c79b47513a9a7dd5023e1c
Jane-Ngala <99291087+Jane-Ngala@users.noreply.github.com>
2022-07-18 08:32:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ if navigation == "Politics Today":
             st.error('Sorry we do not have data for this period. For more objective analysis try inputing dates from 10-06-2022')
         
         # Start date
-         end = st.date_input(label='End: ', value=datetime.datetime(year=2022, month=5, day=30, hour=16, minute=30), key='#end', help="The end date time", on_change=lambda : None)
+        end = st.date_input(label='End: ', value=datetime.datetime(year=2022, month=5, day=30, hour=16, minute=30), key='#end', help="The end date time", on_change=lambda : None)
 
         if start < end and start >= time:
             st.success('Start date: `%s`\n\nEnd date:`%s`' % (start, end))
